bac98f064e6b12a84411636a4fba342e3493196f
sanjibmaity2022 <98643562+sanjibmaity2022@users.noreply.github.com>
2022-02-02 16:38:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+var trex ,trex_running;
+var groundImage
+function preload(){
+  
+}
+
+function setup(){
+ 
+
+}
+
+
+
+
+
+function draw(){
+
+background("green")
+
+drawSprites();
+
+}
+
+
+
+
+
+
+
+
+
